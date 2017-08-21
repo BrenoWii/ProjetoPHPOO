253698a49a01bae3af100a8175e9da831008ca75
BrenoOliveira <brenoo194@gmail.com>
2017-08-21 19:05:42 -0300

    todos arquivos

---
+++ public/bootstrap/bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+use App\Classes\Template;
+use App\Classes\Parameters;
+
+
+
+
+
+$template = new Template;
+
+$twig=$template->init();
+
+$callController = new App\Controllers\Controller;
+$calledController = $callController->controller();
+$controller = new $calledController();
+$controller->setTwig($twig);
+
+
+/**
+ * Chamando o metodo digitado na url
+ *  http://localhost:8888/controller/metodo
+ */
+$callMethod = new App\Controllers\Method;
+$method = $callMethod->method($controller);
+
+
+
+
+/**
+ * Chamando o controller atraves da classe controller e da classe method
+ */
+$parameters= new Parameters;
+$parameter = $parameters->getParameterMethod($controller, $method);
+$controller->$method($parameter);
